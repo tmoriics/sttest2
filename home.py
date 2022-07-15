a97e84ab5ba285a668571ea4e48f4ea27513be78
tmoriics <tmoriics-tky@umin.ac.jp>
2022-07-15 11:06:54 +0900

    Changed containers and empties.

--- home.py
+++ home.py
@@ -20,7 +20,6 @@ import cv2
 import requests
 import altair as alt
 import streamlit as st
-# import folium
 
 
 #####
@@ -53,12 +52,12 @@ def convert_df_to_csv(df):
 
 #####
 #####
-# Main Script
+# Main script
 #####
 #####
 
 ###
-# Title section
+# Title 
 ###
 st.set_page_config(layout='wide', page_title='Diary manager')
 hide_menu_style = """
@@ -67,26 +66,41 @@ hide_menu_style = """
       </style>
       """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.title('排尿日誌マネージャー')
+st.text('Copyright (c) 2022 tmoriics')
 
-lc, rc = st.columns(2)
 
-lc.title('排尿日誌マネージャー')
-lc.text('Diary manager @tmoriics')
-lc.markdown('Copyright (c) 2022 tmoriics')
-
+lcol, rcol = st.columns(2)
+###
+### Today
+###
 dt_now = datetime.datetime.now()
-rc.markdown('### 現在の日時：')
-rc.text(dt_now.strftime('%Y年%m月%d日 %H:%M:%S'+'です。'))
-
+rcol.text('現在の日時：'+dt_now.strftime('%Y年%m月%d日 %H:%M:%S'+'です。'))
 
 ###
-### Date
+# Check point
 ###
-diary_date = st.date_input("日誌の日付を西暦で入力してください。")
-if not diary_date:
-    st.warning('日付の入力を御願いします。')
+e = st.sidebar.empty()
+c = e.container()
+guess = c.text_input("What is the password?")
+if guess != st.secrets["password"]:
+    c.warning("Please input the password.")
     st.stop()
-st.success('入力が確認できました。')
+c.success('Thank you for inputting the password.')
+time.sleep(1)    
+e.empty()
+
+
+###
+### Diary date input
+###
+lc = lcol.container()
+diary_date = lc.date_input("日誌の日付を西暦で入力してください。",
+                           (dt_now+datetime.timedelta(days=3)).date())
+if diary_date == (dt_now+datetime.timedelta(days=3)).date(): 
+    lc.warning('日付の入力を御願いします。')
+    st.stop()
+lc.success('入力が確認できました。'+diary_date.strftime('%Y年%m月%d日'))
 # diary_year_string = '2022'
 # diary_month_string = '5'
 # diary_day_string = '1'
@@ -95,7 +109,6 @@ st.success('入力が確認できました。')
 #                          month=int(diary_month_string), day=int(diary_day_string))
 
 
-st.markdown('### 起床時刻・就寝時刻・翌日起床時刻・翌日就寝時刻（仮）：')
 ###
 ### Wakeup and bed
 ###
@@ -109,8 +122,6 @@ if wakeup_hour_pm_adjust_boolean:
         diary_date, wakeup_time_tmp) + datetime.timedelta(hours=12)
 else:
     wakeup_datetime = datetime.datetime.combine(diary_date, wakeup_time_tmp)
-st.text('対象日の起床時刻は' + wakeup_datetime.strftime("%Y-%m-%dT%H:%M") + 'です．')
-st.text('今と対象日の起床時刻の差は' + str(dt_now-wakeup_datetime) + 'です．')
 bed_hour_string = '9'
 bed_minute_string = '00'
 bed_time_pm_adjust_boolean = True
@@ -143,34 +154,12 @@ if next_bed_time_pm_adjust_boolean:
 else:
     next_bed_datetime = datetime.datetime.combine(
         diary_date + datetime.timedelta(days=1), next_bed_time_tmp)
-
-st.text('対象日の就寝時刻は' + bed_datetime.strftime("%Y-%m-%dT%H:%M") + 'です．')
-st.text('今と対象日の就寝時刻の差は' + str(dt_now-bed_datetime) + 'です．')
-st.text('対象日の就寝時刻と起床時刻の差は' + str(bed_datetime-wakeup_datetime) + 'です．')
-st.text('翌朝の起床時刻と就寝時刻の差（睡眠時間）' + str(next_wakeup_datetime - bed_datetime) + 'です．')
-st.text('翌日の就寝時刻と起床時刻の差は' + str(next_bed_datetime - next_wakeup_datetime) + 'です．')
+st.markdown('### 起床時刻：')
+st.text('対象日の起床時刻は' + wakeup_datetime.strftime("%Y-%m-%dT%H:%M") + 'です．')
 
 
 ###
-# Check point
-###
-guess = st.text_input("What is the password?")
-if guess != st.secrets["password"]:
-    st.warning("Please input the password.")
-    st.stop()
-st.success('Thank you for inputting the password.')
-
-
-###
-# Folium test
-###
-# folium_map = folium.Map(location=[35.69092, 139.700258], zoom_start=15)
-# folium.Marker(location=[35.690921, 139.700258]).add_to(folium_map)
-# folium_map
-
-
-###
-# Sidebar setup
+# Sidebar
 ###
 #
 # sidebar
@@ -184,30 +173,20 @@ display_recognized_image = st.sidebar.checkbox(
 
 
 ###
-# Read diary
+# Diary read
 ###
 #
-# Diary data upload section
-#
+# Diary image(s) upload
 """
 ## 日誌画像アップロード
 """
-#####
-#
-# A
-#
-# img = Image.open('images/samp1.jpg')
-#####
-#
+# A Image.open('images/samp1.jpg')
 # B
-#
 img = Image.open('images/diary_form1_sample1.png')
-
 ri = st.radio("日誌画像をアップロードしてください。スマホカメラでいま撮影しても構いません。", ('PDFファイル', '画像ファイル', 'カメラ'), horizontal=True)
-
 if ri == 'PDFファイル':
     st.write('このような日誌画像をアップロードしてください．')
-    st.image(img, caption='日誌画像例', width=128)
+    st.image(img, caption='日誌画像例', width=160)
     uploaded_file = upload_file_func()
     if uploaded_file is not None:
         st.success('日誌画像が登録されました．')
@@ -218,7 +197,7 @@ if ri == 'PDFファイル':
         st.image(dimg, caption='Uploaded image', use_column_width=True)
 elif ri == '画像ファイル':
     st.write('このような日誌画像をアップロードしてください．')
-    st.image(img, caption='日誌画像例', width=128)
+    st.image(img, caption='日誌画像例', width=160)
     uploaded_jpgfiles = upload_jpgfiles_func()
     if uploaded_jpgfiles is not None:
         for uploaded_jpgfile in uploaded_jpgfiles:
@@ -231,7 +210,7 @@ elif ri == '画像ファイル':
                      use_column_width=True)
 else:
     st.write('このような日誌画像を縦長で撮影してください．')
-    st.image(img, caption='日誌画像例', width=128)
+    st.image(img, caption='日誌画像例', width=160)
     image_file_buffer = take_photo_func()
 #   camera_image = st.camera_input("Take a snapshot of today's diary")
 #   if camera_image:
@@ -247,7 +226,8 @@ else:
                                  file_name="diary.jpg",
                                  mime="image/jpg")
 
-        
+#
+# Recognized image(s)
 if display_recognized_image:
     # if uploaded_files is not None or image_file_buffer is not None:
     st.write('認識された日誌画像（あくまで例です）')
@@ -263,25 +243,17 @@ if display_recognized_image:
 
 
 ###
-# Display recognized result(s)
+# Recognized document
 ###
 """
 # 日誌データ表示（認識結果）
 """
-#####
-#
 # A
-#
 # urination_data_df = pd.read_csv('data/m1modified.csv', sep=',')
 # urination_data_df = pd.read_csv('data/virtually_recognized_m1modified.csv', sep=',')
 # urination_data_df = pd.read_csv('data/recognized_m1modified.csv', sep=',')
-# st.write(
-#     pd.DataFrame(urination_data_df)
-# )
-#####
-#
+# st.write(   pd.DataFrame(urination_data_df))
 # B
-#
 urination_data_df = pd.DataFrame(np.arange(13*8).reshape(13, 8),
                                  columns=['時', '分', '排尿量', 'もれ', '尿意', '切迫感', '残尿感', 'メモ'])
 urination_data_df.loc[:] = [
@@ -299,11 +271,23 @@ urination_data_df.loc[:] = [
     ['', '', '', '無・少量・中量・多量', '有・無', '有・無', '有・無', ''],
     ['', '', '', '無・少量・中量・多量', '有・無', '有・無', '有・無', '']]
 
+### small checks
 # st.write(urination_data_df['もれ'])
 # st.write(urination_data_df['もれ'] == '有')
 # st.write(urination_data_df['もれ'] == '無')
 # st.write(urination_data_df[urination_data_df['もれ'] == '有'])
 # st.write(urination_data_df[urination_data_df['もれ'] == '無'])
+
+
+###
+### Date and time adjustment (Day and hour)
+###
+##########
+# 2022-07-14T16:00 修正
+#  st.write("<font color='red'>最初の行が深夜０時以降だと日付けがおかしくなる｡修正中</font>", unsafe_allow_html=True)
+# 2022-07-14T17:15 修正
+#  st.write("<font color='red'>零時以降が翌日日付けにならない｡修正中</font>", unsafe_allow_html=True)
+##########
 urination_data_df['時'].replace('', np.nan, inplace=True)
 urination_data_df['分'].replace('', np.nan, inplace=True)
 urination_data_df['year'] = diary_date.strftime("%Y")
@@ -381,84 +365,38 @@ urination_data_df.drop(columns=[
                        'datetime_tmp', 'datetime_tmp_before', 'datetime_tmp_after_check'], inplace=True)
 # st.write(urination_data_df)
 
+
+###
+### Downloadable recognized document
+###
 ud_df1 = urination_data_df.drop(columns=['時', '分', 'year', 'month', 'day',
                                          'hour', 'minute',
                                          'micturition',
                                          'leakage', 'desire', 'urgency', 'remaining',
                                          'memo'])
 ud_df = ud_df1.dropna(subset=['datetime'])
-st.dataframe(ud_df.style.highlight_max(axis=0))
+# st.dataframe(ud_df.style.highlight_max(axis=0))
 st.table(ud_df.style.highlight_max(axis=0))
 
 
-##########
-# 2022-07-14T16:00 修正
-#  st.write("<font color='red'>最初の行が深夜０時以降だと日付けがおかしくなる｡修正中</font>", unsafe_allow_html=True)
-# 2022-07-14T17:15 修正
-#  st.write("<font color='red'>零時以降が翌日日付けにならない｡修正中</font>", unsafe_allow_html=True)
-##########
-
-
-##
-# Download DataFrame as a CSV
+###
+# Downloadable recognized CSV
+###
 """
 # CSV形式でのダウンロード
 """
 urination_data_csv = convert_df_to_csv(urination_data_df)
 st.download_button(label="Download data as CSV",
                    data=urination_data_csv,
-                   file_name='u_data.csv',
+                   file_name='ud_data.csv',
                    mime='text/csv')
 
-##
-# グラフ
+###
+# Micturition graph
+###
 """
 # 当該日の尿量・漏れ量グラフ
 """
-# st.write(
-#     pd.DataFrame({
-#         'Date': ['2022-06-24', '2022-06-25', '2022-06-25', '2022-06-26', '2022-06-26'],
-#         'Time': ['20:50:00', '07:10:45', '20:44:00', '07:08:00', '20:40:45'],
-#         'Volume': [200, 150, 250, 140, 210]
-#     })
-# )
-# chart_df = pd.DataFrame(
-#     pd.DataFrame({
-#         'Volume': [200, 150, 250, 140, 210],
-#         'Leakage': [30, 40, 0, 70, 20]
-#     })
-# )
-# st.line_chart(chart_df)
-#####
-#
-# A
-#
-# vol_df = pd.DataFrame(
-#     {
-#         'Volume': [200, 50, 210, 150, 190, 70, 220, 150, 90, 220],
-#         'Leakage': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     }
-# )
-# vol_df['Time'] = ["0:30", "4:30", "7:10", "9:00",
-#                   "11:50", "13:30", "14:30", "17:00", "19:15", "20:50"]
-# chart_df = pd.melt(vol_df, id_vars=['Time'],
-#                    var_name='parameter', value_name='value')
-# c = alt.Chart(chart_df, background=graph_background_color,
-#               title='Volume and leak').mark_line().encode(x='Time', y='value', color='parameter')
-# st.altair_chart(c, use_container_width=True)
-# st.write(pd.DataFrame(vol_df))
-#####
-#
-# B
-#
-# vol_df = pd.DataFrame(
-#     {
-#         'Time': ["8:00", "10:30", "13:00", "17:00", "19:55", "22:30", "1:30", "7:00", "", "", "", "", ""],
-#         'Volume': [100, 250, 300, 225, 125, 100, 150, 200, np.nan, np.nan, np.nan, np.nan, np.nan]
-#     }
-# )
-# vol_df['Leakage'] = [0, 0, 0, 0, 0, 0, 0, 0,
-#                      np.nan, np.nan, np.nan, np.nan, np.nan]
 vol_df = urination_data_df[['datetime', 'micturition', 'leakage']]
 vol_df['datetime_Japan'] = urination_data_df['datetime'].dt.tz_localize(
     'Asia/Tokyo')
@@ -471,37 +409,14 @@ st.altair_chart(c, use_container_width=True)
 st.write(pd.DataFrame(vol_df))
 
 
-#####
-#####
-# テスト残骸
-#####
-#####
-# #
-# # インタラクティブな操作のテスト
-# #
-# """
-# # インタラクティブな操作のテスト
-# """
-# option = st.sidebar.selectbox(
-#     "好きな数字を教えてください，",
-#     list(range(1,11))
-#     )
-# cond = st.sidebar.slider('今の調子は？', 0, 100, 50)
-# text = st.sidebar.text_input('あなたの趣味を教えてください．', value="　　　")
-# si = st.sidebar.checkbox('Show Image')
-# 'あなたの好きな数字は，', option, 'です．'
-# 'コンディションは：', cond
-# 'あなたの趣味は，', text, 'のようです．'
-# if si:
-#     st.image(img, caption='AI Center', use_column_width=True)
-#
-# left_column, right_column = st.columns(2)
-# bi = left_column.button('ボタンを押すと，右側に文字を表示します．')
-# if bi:
-#     right_column.write('ここは右カラム')
-#
-# ex1 = st.expander('問い合わせ1')
-# ex1.write('問合せ1')
-# ex2 = st.expander('問い合わせ2')
-# ex2.write('問合せ2')
-#
+###
+### 起床時刻等
+###
+st.markdown('### 起床時刻・就寝時刻・翌日起床時刻・翌日就寝時刻：')
+st.text('対象日の起床時刻は' + wakeup_datetime.strftime("%Y-%m-%dT%H:%M") + 'です．')
+st.text('対象日の就寝時刻は' + bed_datetime.strftime("%Y-%m-%dT%H:%M") + 'です．')
+st.text('対象日の就寝時刻と起床時刻の差は' + str(bed_datetime-wakeup_datetime) + 'です．')
+st.text('翌朝の起床時刻と就寝時刻の差（睡眠時間）' + str(next_wakeup_datetime - bed_datetime) + 'です．')
+st.text('翌日の就寝時刻と起床時刻の差は' + str(next_bed_datetime - next_wakeup_datetime) + 'です．')
+
+
