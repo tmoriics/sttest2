1cd3d02ae74fc7111fcf1cb8e2fac2ec5decaf43
tmoriics <tmoriics-tky@umin.ac.jp>
2022-07-17 19:29:44 +0900

    Changed secret infomartion.

--- streamlit_setup.sh
+++ streamlit_setup.sh
@@ -4,12 +4,13 @@ echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
+
 [theme]
-primaryColor="#F63366"
-backgroundColor="#88FF88"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif
+primaryColor='#F63366'
+backgroundColor='#88FF88'
+secondaryBackgroundColor='#F0F2F6'
+textColor='#262730'
+font=;sans serif'
 " > ~/.streamlit/config.toml
 
 
