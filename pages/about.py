136b5f84b1172afd0741e54a4eab34c843da27e5
tmoriics <tmoriics-tky@umin.ac.jp>
2022-07-19 00:33:32 +0900

    Updated information.

--- pages/about.py
+++ pages/about.py
@@ -2,7 +2,9 @@
 ##### about.py
 ##### 
 ##### 2022-07-15T06:00
+##### 2022-07-19T00:27
 #####
+
 ### 
 # Imports
 ###
@@ -19,6 +21,9 @@ import streamlit as st
 # Functions
 #####
 #####
+#
+# None for 2022-07-19T00:27
+
 
 #####
 #####
@@ -27,41 +32,59 @@ import streamlit as st
 #####
 
 ###
-# Preload
-###
-img = Image.open('images/aicenter.jpg')
+### Main
+### 
+def main():
+    
+    ###
+    # Preload
+    ###
+    img = Image.open('images/aicenter.jpg')
 
-###
-# Title section
-###
-st.set_page_config(page_title='About this site')
-hide_menu_style = """
+    ###
+    # Title section
+    ###
+    
+    st.set_page_config(page_title='About this site')
+    hide_menu_style = """
       <style>
       #MainMenu {visibility: hidden;}
       </style>
       """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-st.title('このサイトについて')
-st.write('About this site @tmoriics')
-st.image(img, caption='AI Center of the University of Tokyo', use_column_width=False)
-st.markdown('Copyright: ')
-st.text('(c) 2022 tmoriics')
-st.markdown('## アプリの機能：')
-st.text('排尿日誌の認識とそれに基づく指標演算を行います。')
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+    
+    st.title('このサイトについて')
+    st.write('About this site @tmoriics')
+    st.image(img, caption='AI Center of the University of Tokyo', use_column_width=False)
+    
+    st.markdown('Copyright: ')
+    st.text('(c) 2022 tmoriics')
+    
+    st.header('About: ')
+    st.subheader('アプリの機能：')
+    st.text('排尿日誌の認識とそれに基づく指標演算を行います。')
+    
+    
+    ###
+    # Progress bar
+    ###
+    bar = st.progress(0)
+    frame_text = st.empty()
+    for i in range(100):
+        bar.progress(i)
+        frame_text.text("%i/100" % (i + 1))
+        time.sleep(0.05)
+    frame_text.text("Now you can proceed.")
+    time.sleep(0.5)
+    frame_text.empty()
+    bar.empty()
 
 
 ###
-# Progress bar
 ###
-bar = st.progress(0)
-frame_text = st.empty()
-for i in range(100):
-    bar.progress(i)
-    frame_text.text("%i/100" % (i + 1))
-    time.sleep(0.05)
-frame_text.text("Now you can proceed.")
-time.sleep(0.5)
-bar.empty()
-frame_text.empty()
+###
+if __name__ == '__main__':
+    main()
+
 
 
